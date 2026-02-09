e08906b30603004ca649c239e0de95cdce14193d
SeokJun Lee <liberties24601@gmail.com>
2026-02-09 18:40:46 +0900

    0209-3

--- check_asset_path.py
+++
@@ -1,7 +0,0 @@
-try:
-    from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
-    print(f"ISAACLAB_NUCLEUS_DIR: {ISAACLAB_NUCLEUS_DIR}")
-except ImportError:
-    print("Could not import isaaclab")
-except Exception as e:
-    print(f"Error: {e}")
